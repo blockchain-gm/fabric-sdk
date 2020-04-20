023e68ff4690ab827bb41eeefb66d42f9db82e58
liuhy <liu_hang_yu@163.com>
2020-04-20 09:17:47 +0800

    up

--- example/ca.go
+++ example/ca.go
@@ -1,13 +1,8 @@
 package main
 
 import (
-	"crypto/ecdsa"
-	"crypto/x509"
-	"encoding/base64"
-	"encoding/pem"
-	"errors"
-	"fabric-sdk/ca"
 	"fabric-sdk/fabric-ca/api"
+	"fabric-sdk/libca"
 	"fmt"
 	"math/rand"
 	"time"
@@ -46,117 +41,15 @@ func RandString(len int) string {
 // 	return base64.StdEncoding.EncodeToString(key), nil
 // }
 
-type CaClient struct {
-	Cli *ca.CAClientImpl
-}
-
-func NewCaClient() (*CaClient, error) {
-	ca, err := ca.NewCAClient("org1", "mspOrg1", "ca-org1", "./keys/signcerts", &ca.EnrollCredentials{EnrollID: "root", EnrollSecret: "adminpw"})
-	if err != nil {
-		return nil, err
-	}
-	return &CaClient{Cli: ca}, nil
-}
-
-func (ca *CaClient) GetPubKey(ID string) (string, []byte, error) {
-	if ca != nil && ca.Cli != nil {
-		user, err := ca.Cli.GetSigningIdentity(ID)
-		certBytes := user.EnrollmentCertificate()
-		if err != nil || certBytes == nil {
-			return "", nil, err
-		}
-
-		decoded, _ := pem.Decode(certBytes)
-		if decoded == nil {
-			return "", nil, errors.New("Failed cert decoding")
-		}
-
-		x509Cert, err := x509.ParseCertificate(decoded.Bytes)
-		if err != nil {
-			return "", nil, fmt.Errorf("failed to parse certificate: %s", err)
-		}
-		if x509Cert.PublicKeyAlgorithm == x509.ECDSA {
-			ecdsaPublicKey := x509Cert.PublicKey.(*ecdsa.PublicKey)
-			x509EncodedPub, _ := x509.MarshalPKIXPublicKey(ecdsaPublicKey)
-			pemEncodedPub := pem.EncodeToMemory(&pem.Block{Type: "PUBLIC KEY", Bytes: x509EncodedPub})
-			return x509Cert.PublicKeyAlgorithm.String(), pemEncodedPub, nil
-		} else {
-			return "", nil, fmt.Errorf("%s", "unknow algorithm parse error")
-		}
-	}
-	return "", nil, fmt.Errorf("%s", "invalid parameter")
-}
-
-func (ca *CaClient) GetPriKey(ID string) ([]byte, string, error) {
-	if ca != nil && ca.Cli != nil {
-		prikey, ski, err := ca.Cli.GetUserPriKey(ID)
-		if err != nil {
-			return nil, ski, err
-		}
-		return prikey, ski, nil
-	}
-	return nil, "", fmt.Errorf("%s", "invalid parameter")
-}
-
-func (ca *CaClient) GetUserCertificate(id string) (*x509.Certificate, []byte, error) {
-	return ca.Cli.GetUserCertificate(id)
-}
-
-func (ca *CaClient) Sign(id string, msg []byte) (string, error) {
-	signature, err := ca.Cli.Sign(id, msg, "SHA256")
-	if err != nil {
-		return "", err
-	}
-	return base64.StdEncoding.EncodeToString(signature), nil
-}
-
-func (ca *CaClient) Verify(id string, msg []byte, signature string) error {
-	//解码
-	signBytes, err := base64.StdEncoding.DecodeString(signature)
-	if err != nil {
-		return err
-	}
-	return ca.Cli.Verify(id, msg, signBytes, "SHA256")
-}
-
-func (ca *CaClient) Register(request *api.RegistrationRequest) (string, error) {
-	var a []api.Attribute
-	for i := range request.Attributes {
-		a = append(a, api.Attribute{Name: request.Attributes[i].Name, Value: request.Attributes[i].Value, ECert: request.Attributes[i].ECert})
-	}
-
-	r := api.RegistrationRequest{
-		Name:           request.Name,
-		Type:           request.Type,
-		MaxEnrollments: request.MaxEnrollments,
-		Affiliation:    request.Affiliation,
-		Attributes:     a,
-		CAName:         request.CAName,
-		Secret:         request.Secret,
-	}
-	return ca.Cli.Register(&r)
-}
-
-func (ca *CaClient) Enroll(request *api.EnrollmentRequest) error {
-	return ca.Cli.Enroll(request)
-}
-
-func (ca *CaClient) GetIdentity(id, caname string) (*api.IdentityResponse, error) {
-	return ca.Cli.GetIdentity(id, caname)
-}
-
-func (ca *CaClient) GetCAInfo() error {
-	caInfo, err := ca.Cli.GetCAInfo()
-	if err != nil {
-		return err
-	}
-	fmt.Println("caInfo:", caInfo.CAName, caInfo.Version)
-
-	return nil
-}
-
 func main() {
-	ca, err := NewCaClient()
+	config, err := libca.LoadDBConfig("./")
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	fmt.Println(config)
+
+	ca, err := libca.NewCaClient()
 	if err != nil {
 		fmt.Println(err.Error())
 		return
---
+++ libca/config.go
@@ -0,0 +1,145 @@
+package libca
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+)
+
+const (
+	SEC_TYPE = "SW"
+
+	SW_PROVIDER = "SW"
+	SW_HASHALGO = "SHA2"
+	SW_LEVEL    = 256
+
+	GM_PROVIDER = "GM"
+	GM_HASHALGO = "gmsm3"
+	GM_LEVEL    = 256
+
+	ORG_NAME      = "org1"
+	ORG_MSPID     = "mspOrg1"
+	CA_NAME       = "ca-org1"
+	KEY_STOREPATH = "keys"
+	ENROLL_ID     = "root"
+	ENROLL_SECRET = "adminpwd"
+	CA_ID         = "ca.org1.example.com"
+	URL           = "http://ca.org1.example.com:7054"
+	SSL_OVERRIDE  = "127.0.0.1"
+
+	TLS_CA_SERVER_CERT = "./configs/ssl/ca.org1.example.com-cert.pem"
+	TLS_CA_CLIENT_KEY  = "./configs/ssl/00a81ff19d1fc744d5dc8c20f5bf61488ce6a9714f080642449c8327695e5789_sk"
+	TLS_CA_CLIENT_CERT = "./configs/ssl/ca.org1.example.com-cert.pem"
+)
+
+type BCCSP struct {
+	Provider string //defalut SW
+	HashAlgo string //default SHA2
+	Level    int    //256
+}
+
+type CaTLS struct {
+	ServerCertPath string
+	ClientKeyPath  string
+	ClientCertPath string
+}
+
+type CaConfig struct {
+	OrgName      string
+	OrgMSPID     string
+	CaName       string
+	KeyStorePath string
+	EnrollID     string
+	EnrollSecret string
+	CaID         string
+	URL          string
+	SSLOverride  string
+	TLS          *CaTLS
+}
+
+type FabConfig struct {
+	SecType string
+	SW      BCCSP
+	GM      BCCSP
+	Ca      map[string]CaConfig
+}
+
+func CheckFileIsExist(filename string) bool {
+	var exist = true
+	if _, err := os.Stat(filename); os.IsNotExist(err) {
+		exist = false
+	}
+	return exist
+}
+
+func LoadDBConfig(parentPath string) (*FabConfig, error) {
+	path := filepath.Join(parentPath, "configs/fabconf.json")
+	fpath, err := filepath.Abs(path)
+	if err != nil {
+		return nil, err
+	}
+
+	config := new(FabConfig)
+	if CheckFileIsExist(fpath) { //文件存在
+		bs, err := ioutil.ReadFile(fpath)
+		if err != nil {
+			return nil, err
+		}
+		err = json.Unmarshal(bs, config)
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		config.SecType = SEC_TYPE
+		config.SW = BCCSP{
+			Provider: SW_PROVIDER,
+			HashAlgo: SW_HASHALGO,
+			Level:    SW_LEVEL,
+		}
+		config.GM = BCCSP{
+			Provider: GM_PROVIDER,
+			HashAlgo: GM_HASHALGO,
+			Level:    GM_LEVEL,
+		}
+
+		config.Ca = map[string]CaConfig{
+			CA_NAME: CaConfig{
+				OrgName:      ORG_NAME,
+				OrgMSPID:     ORG_MSPID,
+				CaName:       CA_NAME,
+				KeyStorePath: KEY_STOREPATH,
+				EnrollID:     ENROLL_ID,
+				EnrollSecret: ENROLL_SECRET,
+				CaID:         CA_ID,
+				URL:          URL,
+				SSLOverride:  SSL_OVERRIDE,
+				TLS: &CaTLS{
+					ServerCertPath: TLS_CA_SERVER_CERT,
+					ClientKeyPath:  TLS_CA_CLIENT_KEY,
+					ClientCertPath: TLS_CA_CLIENT_CERT,
+				},
+			},
+		}
+
+		data, err := json.MarshalIndent(config, "", "   ")
+		if err != nil {
+			return nil, err
+		}
+
+		err = ioutil.WriteFile(fpath, data, 0666)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return config, nil
+}
+
+// func main() {
+// 	config, err := LoadDBConfig("./")
+// 	if err != nil {
+// 		fmt.Println(err.Error())
+// 		return
+// 	}
+// 	fmt.Println(config)
+// }
